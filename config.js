cee0fd6343d60b86eb483236eee87723f96ff31e
Chakrawat2003 <gungan2011ca@gmail.com>
2026-09-02 16:24:41 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.APP_CONFIG={
+  SUPABASE_URL:"https://bqfmwybksbmpitaoowuq.supabase.co",
+  SUPABASE_ANON_KEY:"sb_publishable_xMMW1xMpE1beeVuSUksV0A_qy_FjbRK",
+  ESRI_API_KEY:"YOUR_ESRI_API_KEY"
+};
